f62aa067f9e95df23cc651b3e8eb5f5369b651ef
Sébastien Lavoie <seblav02@gmail.com>
2016-08-06 15:04:00 -0700

    Initial Commit

---
+++ classes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  classes
+//
+//  Created by Sebastien Lavoie on 2016-08-06.
+//  Copyright © 2016 Sebastien Lavoie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
